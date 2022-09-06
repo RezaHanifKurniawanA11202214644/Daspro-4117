6bee05c0da6efbbe3a5f61ea99088a8774885e3d
RezaHanifKurniawanA11202214644 <112889861+RezaHanifKurniawanA11202214644@users.noreply.github.com>
2022-09-06 19:08:27 +0700

    Add files via upload
    Tugas 1 
    Membuat project C++ berisi Nama, NIM, Asal sekolah.

---
+++ Tugas1.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "DATA DIRI SAYA " << endl;
+    cout << "Nama              : Reza Hanif Kurniawan" << endl;
+    cout << "NIM               : A11.2022.14644" << endl;
+    cout << "Asal Sekolah      : SMA N 1 Pegandon" << endl;
+
+}
