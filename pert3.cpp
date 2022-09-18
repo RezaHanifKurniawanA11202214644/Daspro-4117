a808db71d485811a9046d921140a858216b8062c
RezaHanifKurniawanA11202214644 <112889861+RezaHanifKurniawanA11202214644@users.noreply.github.com>
2022-09-18 10:41:17 +0700

    Add files via upload

---
+++ pert3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int x = 2000;
+    cout << "Hello World!!! " << x << endl;
+    /* comment */
+    string nama;
+    cout << "\nTuliskan Nama: ";
+    cin >> nama;
+    cout << "Hii " <<nama << endl;
+    cout << "Selamat Datang !!!" << endl;
+    cout << "\nNama Saya Reza Hanif Kurniawan" << endl;
+
+    int u = 18;
+    float t = 174.5;
+    char c = 'L';
+    string ub = "XL";
+    cout << "\nUmur: " << u;
+    cout << "\nTinggi: " << t;
+    cout << "\nUkuran Celana: " << c;
+    cout << "\nUkuran Baju: " << ub;
+
+    return 0;
+
+
+}
