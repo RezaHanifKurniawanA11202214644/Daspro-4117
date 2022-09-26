84b8136837746c10c0935f761fa0bc9ab4e283e0
RezaHanifKurniawanA11202214644 <112889861+RezaHanifKurniawanA11202214644@users.noreply.github.com>
2022-09-26 23:12:46 +0700

    Update Pertemuan 5.cpp

--- Pertemuan 5.cpp
+++ Pertemuan 5.cpp
@@ -45,22 +45,43 @@ int main()
 
 //Luas Lingkaran
 
+    /*
     //cara 1 memakai phi 3.14
+
     float phi, r1, r2, hasil;
     phi= 3.14;
     r1= 10;
     r2= 10;
     hasil= phi * r1 * r2;
-    cout << "Hasil: " << hasil << endl;
 
+    cout << "\ta. Masukan phi: ";
+    cin >> phi;
+    cout << "\tb. Masukan r1: ";
+    cin >> r1;
+    cout << "\tc. Masukan r2: ";
+    cin >> r2;
+    cout << "\td. Masukan rumus: phi * r1 * r2" << endl;
+    cout << "\t --> Hasil perhitungan: " << phi * r1 * r2 << endl;
+    cout << endl;
+    */
 
     //cara 2 memakai phi 22/7
+
     float phi, r1, r2, hasil;
     phi= 22;
     r1= 14;
     r2= 14;
-    hasil= (phi * r1 * r2) / 7;
-    cout << "hasil: " << hasil << endl;
+    hasil= (phi * r1 * r2);
+
+    cout << "\ta. Masukan phi: ";
+    cin >> phi;
+    cout << "\tb. Masukan r1: ";
+    cin >> r1;
+    cout << "\tc. Masukan r2: ";
+    cin >> r2;
+    cout << "\td. Masukan rumus: (phi * r1 * r2) / 7" << endl;
+    cout << "\t --> Hasil perhitungan: " << (phi * r1 * r2) / 7 << endl;
+    cout << endl;
 
     return 0;
 }
