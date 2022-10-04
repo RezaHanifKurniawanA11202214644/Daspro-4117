0ebb7c8eff8b7b3a4c37c56e4a2bd48841a9db95
RezaHanifKurniawanA11202214644 <112889861+RezaHanifKurniawanA11202214644@users.noreply.github.com>
2022-10-04 13:26:32 +0700

    Add files via upload

---
+++ pertemuan 9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  x = 0;
+
+  do{
+     if (x % 5 == 0) {
+     cout << x << endl;
+     }
+     x++;
+    }while (x<=1000);
+    
+     
+return 0;
+}
+    
